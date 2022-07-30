15ee93966c6fb64659260fdf8c4d35671407dc4c
Benjamin Mecanović <benjamin.mecanovic@superology.com>
2022-07-30 18:01:32 +0200

    Adds .uniqed() on Sequence

---
+++ Data/Utilities/Extensions/Sequence+Uniqued.swift
@@ -0,0 +1,15 @@
+//
+//  Sequence+Uniqued.swift
+//  Data
+//
+//  Created by Benjamin Mecanović on 30.07.2022..
+//
+
+import Foundation
+
+extension Sequence where Iterator.Element: Hashable {
+    func uniqued() -> [Iterator.Element] {
+        var seen: [Iterator.Element: Bool] = [:]
+        return self.filter { seen.updateValue(true, forKey: $0) == nil }
+    }
+}
