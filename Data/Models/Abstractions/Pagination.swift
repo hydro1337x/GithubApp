14f9c09121b9871d480f2bb9ce0e1cc33ce5ae7c
Benjamin Mecanović <benjamin.mecanovic@superology.com>
2022-07-30 12:25:59 +0200

    Adds PaginatedResponse

---
+++ Data/Models/Abstractions/Pagination.swift
@@ -0,0 +1,12 @@
+//
+//  Pagination.swift
+//  Data
+//
+//  Created by Benjamin Mecanović on 30.07.2022..
+//
+
+import Foundation
+
+public protocol Pagination {
+    var total: Int { get }
+}
---
+++ Data/Utilities/Paginator.swift
@@ -0,0 +1,8 @@
+//
+//  Paginator.swift
+//  Data
+//
+//  Created by Benjamin Mecanović on 30.07.2022..
+//
+
+import Foundation
