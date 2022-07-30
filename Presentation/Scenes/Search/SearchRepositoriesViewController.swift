cbfa87a921ae4be89860efd216fd2a4b25fbbd71
Benjamin Mecanović <benjamin.mecanovic@superology.com>
2022-07-30 15:02:58 +0200

    Adds SearchRepositoriesViewController

--- Presentation/Scenes/Search/SearchRepositoriesViewController.swift
+++ Presentation/Scenes/Search/SearchRepositoriesViewController.swift
@@ -6,24 +6,91 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
-class SearchRepositoriesViewController: UIViewController {
+public final class SearchRepositoriesViewController: UIViewController {
+    typealias DataSource = UITableViewDiffableDataSource<String, RepositoryViewModel>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<String, RepositoryViewModel>
 
-    override func viewDidLoad() {
+    let tableView = UITableView()
+
+    private lazy var dataSource = makeDataSource()
+    private let disposeBag = DisposeBag()
+    private let sectionIdentifier = "section"
+    private let itemsIdentifier = "items"
+
+    private let viewModel: SearchRepositoriesViewModel
+
+    public init(viewModel: SearchRepositoriesViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    public override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupLayout()
+        setupStyle()
+        registerCells()
+        setupSubscriptions()
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func registerCells() {
+        tableView.register(RepositoryTableViewCell.self,
+                           forCellReuseIdentifier: String(describing: RepositoryTableViewCell.self))
     }
-    */
 
+    private func setupSubscriptions() {
+        let initialTrigger = Signal.just(())
+
+        let input = SearchRepositoriesViewModel.Input(initialTrigger: initialTrigger)
+
+        let output = viewModel.transform(input: input)
+
+        output.repositories
+            .map(makeSnapshot(with:))
+            .drive(onNext: { [unowned self] snapshot in
+                dataSource.apply(snapshot)
+            })
+            .disposed(by: disposeBag)
+    }
+
+    private func makeDataSource() -> DataSource {
+        DataSource(tableView: tableView) { tableView, indexPath, viewModel in
+
+            let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: RepositoryTableViewCell.self),
+                                                     for: indexPath) as? RepositoryTableViewCell
+            cell?.configure(with: viewModel)
+            return cell
+        }
+    }
+
+    private func makeSnapshot(with repositories: [RepositoryViewModel]) -> Snapshot {
+        var snapshot = Snapshot()
+        snapshot.appendSections([sectionIdentifier])
+        snapshot.appendItems(repositories, toSection: sectionIdentifier)
+        return snapshot
+    }
+
+}
+
+extension SearchRepositoriesViewController: ViewConstructing {
+    func setupLayout() {
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tableView)
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+    }
+
+    func setupStyle() {
+        view.backgroundColor = .red
+    }
 }
