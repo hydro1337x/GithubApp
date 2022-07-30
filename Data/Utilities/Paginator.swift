9e1c9ca71a2417ea62a457fa9e18f5633c36fbc2
Benjamin Mecanović <benjamin.mecanovic@superology.com>
2022-07-30 12:26:08 +0200

    Adds Paginator

--- Data/Utilities/Paginator.swift
+++ Data/Utilities/Paginator.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+public final class Paginator<PageType> {
+    let limit: Int
+    private(set) var currentPage = 0
+    private var hasNextPage = true
+    private var pages: [PageType] = []
+
+    public init(limit: Int) {
+        self.limit = limit
+    }
+
+    func paginate(_ input: PaginatedResponse<PageType>) -> [PageType] {
+        guard hasNextPage else {
+            hasNextPage = false
+            return pages
+        }
+
+        if currentPage * limit < input.pagination.total {
+            currentPage += 1
+            pages.append(contentsOf: input.page)
+            return pages
+        } else {
+            return pages
+        }
+    }
+
+    func resetState() {
+        currentPage = 0
+        hasNextPage = true
+        pages = []
+    }
+}
