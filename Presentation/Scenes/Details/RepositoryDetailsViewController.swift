47b46e094e8ee7c391eea5dba3b81323c5357892
Benjamin Mecanović <benjamin.mecanovic@superology.com>
2022-07-31 20:58:14 +0200

    Adds RepositoryDetailsViewController

--- Presentation/Scenes/Details/RepositoryDetailsViewController.swift
+++ Presentation/Scenes/Details/RepositoryDetailsViewController.swift
@@ -7,23 +7,63 @@
 
 import UIKit
 
-class RepositoryDetailsViewController: UIViewController {
+public final class RepositoryDetailsViewController: UIViewController {
 
-    override func viewDidLoad() {
+    let scrollView = UIScrollView()
+    let stackView = UIStackView()
+    let avatarImageView = AsyncImageView()
+
+    private let viewModel: RepositoryDetailsViewModel
+
+    public init(viewModel: RepositoryDetailsViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    public override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupLayout()
+        setupStyle()
     }
-    
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension RepositoryDetailsViewController: ViewConstructing {
+    func setupLayout() {
+
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(scrollView)
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.addSubview(stackView)
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            stackView.bottomAnchor.constraint(lessThanOrEqualTo: scrollView.bottomAnchor)
+        ])
+
+        avatarImageView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.addArrangedSubview(avatarImageView)
+        NSLayoutConstraint.activate([
+            avatarImageView.heightAnchor.constraint(equalToConstant: 100),
+            avatarImageView.widthAnchor.constraint(equalTo: avatarImageView.heightAnchor)
+        ])
     }
-    */
 
+    func setupStyle() {
+        view.backgroundColor = .red
+        stackView.axis = .vertical
+        stackView.distribution = .equalCentering
+    }
 }
