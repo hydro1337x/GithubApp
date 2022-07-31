e0ed760cd8e7d6b1ab6a24635e2fc19c2b9a96f1
Benjamin Mecanović <benjamin.mecanovic@superology.com>
2022-07-31 21:01:37 +0200

    Moves shared dependencies to SceneDelegate

--- GithubApp/SceneDelegate.swift
+++ GithubApp/SceneDelegate.swift
@@ -16,14 +16,35 @@ import RxRelay
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    var coordinator: Coordinator?
+
     let urlSession = URLSession(configuration: .default)
+    let paginator = Paginator<Repository>(limit: 10, initialPage: 1)
+    lazy var repositoryListRequestMapper = FetchRepositoryListRequestMapper().eraseToAnyMapper
+    lazy var ownerResponseMapper = OwnerResponseMapper().eraseToAnyMapper
+    lazy var repositoryListResponseMapper = RepositoryListResponseMapper(
+        ownerResponseMapper: ownerResponseMapper
+    ).eraseToAnyMapper
+    lazy var fetchRepositoryListRepository = URLSessionFetchRepositoryListRepository(
+        session: urlSession,
+        paginator: paginator,
+        requestMapper: repositoryListRequestMapper,
+        responseMapper: repositoryListResponseMapper
+    )
+    lazy var fetchImageRepository = URLSessionFetchImageRepository(session: urlSession)
+    lazy var fetchImageUseCase = ConcreteFetchImageUseCase(repository: fetchImageRepository)
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
         window = UIWindow(windowScene: windowScene)
-        let rootSceneFactory = RootSceneFactory(session: urlSession)
+        let rootSceneFactory = RootSceneFactory(
+            fetchRepositoryListRepository: fetchRepositoryListRepository,
+            fetchRepositoryRepository: fetchRepositoryListRepository,
+            fetchImageUseCase: fetchImageUseCase
+        )
         let coordinator = RootCoordinator(window: window!, factory: rootSceneFactory)
+        self.coordinator = coordinator
         coordinator.start()
     }
 }
