4f3fde412e4fe2dd9eebf138539bdb6ed55b0a98
Benjamin Mecanović <benjamin.mecanovic@superology.com>
2022-07-31 13:50:47 +0200

    Adds RootSceneFactory

--- GithubApp/Factories/RootSceneFactory.swift
+++ GithubApp/Factories/RootSceneFactory.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+import UIKit
+import RxSwift
+import Domain
+import Data
+import Presentation
+
+struct RootSceneFactory {
+    let session: URLSession
+
+    func makeSearchRepositoresViewController() -> UIViewController {
+        let scheduler = SerialDispatchQueueScheduler(qos: .userInitiated)
+        let paginator = Paginator<Repository>(limit: 10, initialPage: 1)
+        let repositoryListRequestMapper = FetchRepositoryListRequestMapper().eraseToAnyMapper
+        let ownerResponseMapper = OwnerResponseMapper().eraseToAnyMapper
+        let repositoryListResponseMapper = RepositoryListResponseMapper(
+            ownerResponseMapper: ownerResponseMapper
+        ).eraseToAnyMapper
+        let fetchRepositoryListRepository = URLSessionFetchRepositoryListRepository(
+            session: session,
+            paginator: paginator,
+            requestMapper: repositoryListRequestMapper,
+            responseMapper: repositoryListResponseMapper
+        )
+        let fetchImageRepository = URLSessionFetchImageRepository(session: session)
+        let fetchRepositoryListUseCase = ConcreteFetchRepositoryListUseCase(repository: fetchRepositoryListRepository)
+        let fetchImageUseCase = ConcreteFetchImageUseCase(repository: fetchImageRepository)
+        let viewModel = SearchRepositoriesViewModel(
+            fetchRepositoryListUseCase: fetchRepositoryListUseCase,
+            fetchImageUseCase: fetchImageUseCase,
+            scheduler: scheduler
+        )
+        let viewController = SearchRepositoriesViewController(viewModel: viewModel)
+
+        return viewController
+    }
+}
