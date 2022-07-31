b089cf6324b11701477cb7644850bb075b6b9cc6
Benjamin Mecanović <benjamin.mecanovic@superology.com>
2022-07-31 10:54:22 +0200

    Adds concrete fetch image repository

--- Data/Repositories/URLSessionFetchImageRepository.swift
+++ Data/Repositories/URLSessionFetchImageRepository.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+import Domain
+import RxSwift
+
+public final class URLSessionFetchImageRepository: FetchImageRepository {
+
+    private let session: URLSession
+
+    public init(session: URLSession) {
+        self.session = session
+    }
+
+    public func fetch(with input: FetchImageInput) -> Single<Data> {
+        guard let url = URL(string: input.url) else { return .error(URLError(.badURL)) }
+
+        let request = URLRequest(url: url)
+
+        return fetch(request)
+    }
+
+    private func fetch(_ request: URLRequest) -> Single<Data> {
+        Single.create { [weak self] single in
+            guard let self = self else { return Disposables.create() }
+
+            let task = self.session.dataTask(with: request) { data, _, error in
+                guard let data = data else {
+                    return single(.failure(error ?? URLError(.badServerResponse)))
+                }
+
+                single(.success(data))
+            }
+
+            task.resume()
+
+            return Disposables.create {
+                task.cancel()
+            }
+        }
+    }
+}
