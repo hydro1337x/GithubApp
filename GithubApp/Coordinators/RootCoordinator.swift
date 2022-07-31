256b3916792df2b475a8d9dbf074a94bf968d293
Benjamin Mecanović <benjamin.mecanovic@superology.com>
2022-07-31 21:01:21 +0200

    Adds RepositoryDetailsViewController pushing on navigation stack

--- GithubApp/Coordinators/RootCoordinator.swift
+++ GithubApp/Coordinators/RootCoordinator.swift
@@ -9,27 +9,47 @@ import Foundation
 import UIKit
 import RxSwift
 import RxCocoa
+import Presentation
 
 final class RootCoordinator: Coordinator {
     let window: UIWindow
+    let navigationController = UINavigationController()
 
+    private let selectionRelay = PublishRelay<RepositoryViewModel>()
+    private let disposeBag = DisposeBag()
     private let factory: RootSceneFactory
 
     init(window: UIWindow,
-         factory: RootSceneFactory) {
+         factory: RootSceneFactory
+    ) {
         self.window = window
         self.factory = factory
     }
 
     func start() {
+        setupSubscriptions()
         showRootScene()
     }
 
+    private func setupSubscriptions() {
+        selectionRelay
+            .asSignal()
+            .emit(onNext: { [unowned self] repository in
+                showRepositoryDetailsScene(with: repository.id)
+            })
+            .disposed(by: disposeBag)
+    }
+
     private func showRootScene() {
-        let viewController = factory.makeSearchRepositoresViewController()
+        let viewController = factory.makeSearchRepositoresViewController(with: selectionRelay)
         viewController.title = "Search Repositores"
-        let navigationController = UINavigationController(rootViewController: viewController)
+        navigationController.setViewControllers([viewController], animated: false)
         window.rootViewController = navigationController
         window.makeKeyAndVisible()
+    }
+
+    private func showRepositoryDetailsScene(with id: String) {
+        let viewController = factory.makeRepositoryDetailsViewController(with: id)
+        navigationController.pushViewController(viewController, animated: true)
     }
 }
