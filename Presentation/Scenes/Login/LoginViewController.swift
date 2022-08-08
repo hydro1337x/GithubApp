0c370801be3e56182a1d8adc17f38f0d9d40abe1
Benjamin Mecanović <benjamin.mecanovic@superology.com>
2022-08-08 21:28:13 +0200

    Adds secure toggle for password textfield

--- Presentation/Scenes/Login/LoginViewController.swift
+++ Presentation/Scenes/Login/LoginViewController.swift
@@ -10,6 +10,15 @@ import RxSwift
 import RxCocoa
 
 public final class LoginViewController: UIViewController {
+    enum Dimension {
+        static let padding: CGFloat = 36
+        static let textFieldHeight: CGFloat = 44
+        static let buttonHeight: CGFloat = 44
+        static let stackViewSpacing: CGFloat = 8
+        static let stackViewOffset: CGFloat = 50
+        static let subStackViewSpacing: CGFloat = 2
+    }
+
     let scrollView = UIScrollView()
     let stackView = UIStackView()
     let emailTextField = UITextField()
@@ -18,6 +27,7 @@ public final class LoginViewController: UIViewController {
     let passwordValidityLabel = UILabel()
     let loginButton = UIButton(type: .system)
     let activityIndicatorView = UIActivityIndicatorView()
+    let toggleSecureEntryButton = UIButton(type: .system)
 
     private let viewModel: LoginViewModel
     private let disposeBag = DisposeBag()
@@ -101,6 +111,20 @@ public final class LoginViewController: UIViewController {
                 handleLoginState(state)
             })
             .disposed(by: disposeBag)
+
+        toggleSecureEntryButton.rx
+            .tap
+            .scan(true) { previous, _ in
+                !previous
+            }
+            .startWith(true)
+            .asDriver(onErrorDriveWith: .empty())
+            .drive(onNext: { [unowned self] toggle in
+                let image = UIImage(systemName: toggle ? "eye" : "eye.slash")
+                toggleSecureEntryButton.setImage(image, for: .normal)
+                passwordTextField.isSecureTextEntry = toggle
+            })
+            .disposed(by: disposeBag)
     }
 
     private func handleLoginState(_ state: DiscardableDataState) {
@@ -148,21 +172,25 @@ extension LoginViewController: ViewConstructing {
         scrollView.addSubview(stackView)
         stackView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            stackView.topAnchor.constraint(lessThanOrEqualTo: scrollView.topAnchor, constant: 50),
-            stackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 36),
-            stackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -36),
+            stackView.topAnchor.constraint(lessThanOrEqualTo: scrollView.topAnchor, constant: Dimension.stackViewOffset),
+            stackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: Dimension.padding),
+            stackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -Dimension.padding),
             stackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
             stackView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor)
         ])
 
         [emailTextField, passwordTextField].forEach {
-            $0.heightAnchor.constraint(equalToConstant: 44).isActive = true
+            $0.heightAnchor.constraint(equalToConstant: Dimension.textFieldHeight).isActive = true
         }
+
+        passwordTextField.rightView = toggleSecureEntryButton
 
         let emailSubStackView = makeSubStackView(with: emailTextField, emailValidityLabel)
         stackView.addArrangedSubview(emailSubStackView)
         let passwordSubStackView = makeSubStackView(with: passwordTextField, passwordValidityLabel)
         stackView.addArrangedSubview(passwordSubStackView)
+
+        loginButton.heightAnchor.constraint(equalToConstant: Dimension.buttonHeight).isActive = true
         stackView.addArrangedSubview(loginButton)
 
         view.addSubview(activityIndicatorView)
@@ -178,13 +206,14 @@ extension LoginViewController: ViewConstructing {
         view.backgroundColor = .systemBackground
 
         stackView.axis = .vertical
-        stackView.spacing = 8
+        stackView.spacing = Dimension.stackViewSpacing
         stackView.alignment = .fill
         stackView.distribution = .equalSpacing
 
         emailTextField.placeholder = "Email"
 
         passwordTextField.placeholder = "Password"
+        passwordTextField.rightViewMode = .always
 
         [emailTextField, passwordTextField].forEach {
             $0.borderStyle = .roundedRect
@@ -192,21 +221,26 @@ extension LoginViewController: ViewConstructing {
 
         [emailValidityLabel, passwordValidityLabel].forEach {
             $0.font = UIFont.systemFont(ofSize: 12, weight: .light)
+            $0.textColor = UIColor.systemOrange
         }
 
         loginButton.setTitle("Login", for: .normal)
-        loginButton.setTitleColor(.blue, for: .normal)
-        loginButton.backgroundColor = .red
+        loginButton.setTitleColor(.white, for: .normal)
+        loginButton.backgroundColor = .systemBlue
+        loginButton.layer.cornerRadius = Dimension.buttonHeight / 2
+        loginButton.clipsToBounds = true
 
         activityIndicatorView.style = .large
         activityIndicatorView.hidesWhenStopped = true
+
+        toggleSecureEntryButton.imageEdgeInsets = UIEdgeInsets(top: 0, left: -16, bottom: 0, right: 0)
     }
 
     private func makeSubStackView(with views: UIView...) -> UIView {
         let stackView = UIStackView()
 
         stackView.axis = .vertical
-        stackView.spacing = 2
+        stackView.spacing = Dimension.stackViewSpacing
         views.forEach {
             stackView.addArrangedSubview($0)
         }
